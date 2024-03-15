9b2ff30c466ba4cd9a75e00693cf47da60c4cc60
Adithya Behera <96430651+Adithyaa321@users.noreply.github.com>
2024-03-15 11:24:50 +0530

    Why IIFE and why use ; at the end of IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,8 @@
 // Immediately Invoked Function Expressions (IIFE)
-
-
+/* Encapsulation: IIFEs create a new scope for variables, preventing them from polluting the global scope. 
+This helps avoid naming conflicts and unintended interactions with other scripts or libraries.*/
+/* Overall, IIFEs are a powerful tool in JavaScript for creating encapsulated, private scopes,
+initializing scripts, and avoiding common issues like global namespace pollution and hoisting problems. */
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
@@ -8,5 +10,5 @@
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
-
+} )('hitesh');
+//Always end IIFE with a semi colon ; to show end of execution or else you ll get an error
